2ae1120ca58e59b9aa9c27e0fea9dace5ed2c0a0
PraveenKuamrS <116068536+PraveenKuamrS@users.noreply.github.com>
2022-10-18 17:44:41 +0530

    Add files via upload

---
+++ Areas.java
@@ -0,0 +1,44 @@
+package data;
+
+public class Areas {
+
+	public static void main(String[] args) {
+		// rectangle (l*h)
+	int l,h,area;
+	l=4;
+	h=2;
+	area= l*h;
+	System.out.println("The area of rectangle is:"  + area);
+	
+	
+	// square
+	int a,d;
+	a=10;
+	d=a*a;
+	System.out.println("The area of square is:" +d);
+	
+	
+	//triangle
+	float x,y,z;
+	float X=1.5f;
+	int Y=3;
+	z=X*Y/2;
+	System.out.println("The area of triangle is :" +z );
+	
+	
+	//circle
+	int r,o;
+	r=5;
+	o=r*r;
+	System.out.println("The area of circle is:"+o);
+	
+	
+	
+	
+	
+	
+	
+		
+			}
+
+}
